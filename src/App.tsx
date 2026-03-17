420300b3f70b562c48f84537d08319000baebfe2
Ghomari Akram <akram.abdeslem.ghomari@gmail.com>
2026-03-17 02:26:12 +0100

    install dependencies & initial setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-red-500"> Hello world</h1>
+      <button className="bg-blue-500 text-white px-4 py-2 rounded">
+        Click me
+      </button>
+    </>
+  );
+}
+
+export default App;
