5bac1c16a42cf9b9566d0d3346c7ef7352ee9c87
Ghomari Akram <akram.abdeslem.ghomari@gmail.com>
2026-03-17 04:06:27 +0100

    add seed to get stable across re-renders

--- src/features/users/api/usersApi.ts
+++ src/features/users/api/usersApi.ts
@@ -2,11 +2,16 @@ import type { UsersResponse, PaginationParams } from "../types/user.types";
 
 const BASE_URL = "https://randomuser.me/api";
 
+//NOTE: Seed is added to ensure consistent results across pagination (same users on each page on every load) because randomuser.me generates a new set of users for each request.
+// In a real app, the backend should handle this.
+const SEED = "user-directory-app";
+
 export async function fetchUsers({
   page,
   pageSize,
 }: PaginationParams): Promise<UsersResponse> {
   const params = new URLSearchParams({
+    seed: SEED,
     page: String(page),
     results: String(pageSize),
   });
